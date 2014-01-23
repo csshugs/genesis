c41c8609bb4e5905886826b3aa987fec3a5b7f70
DennisHeibuelt <dennis@csshugs.com>
2014-01-23 18:25:09 +0100

    Rearranged

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,45 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
+
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'dist/assets/css/style.css': 'app/assets/css/style.scss'
+                }
+            },
+            compressed: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/assets/css/style.css': 'app/assets/css/style.scss'
+                }
+            }
+        },
+
+        assemble: {
+            options: {
+                flatten: true,
+                layout: 'default.hbs',
+                layoutdir: 'app/templates/layouts',
+                assets: 'dist/assets',
+                partials: ['app/templates/pages/*.hbs', 'app/templates/parts/*.hbs']
+            },
+            dev: {
+                files: {'dist/': ['app/templates/pages/*.hbs' ]},
+            },
+            deploy: {
+                options: {layout: 'default--deploy.hbs' },
+                files: {'dist/': ['app/templates/pages/*.hbs' ]},
+            }
+        },
 
         watch: {
             scss: {
@@ -22,7 +59,7 @@ module.exports = function(grunt) {
                 tasks: 'img'
             },
             sprite: {
-                files: ['app/assets/img/css/sprites/**/{.*,*,*/*}'],
+                files: ['app/assets/img/css/sprites/src/{.*,*,*/*}'],
                 tasks: 'copysprite'
             },
             livereload: {
@@ -33,46 +70,16 @@ module.exports = function(grunt) {
                 files: [
                     'dist/**/*.html',
                     'dist/assets/css/{,*/}*.css',
-                    'dist/assets/js/{,*/}*.js',
+                    'dist/assets/js/*/*.js',
                     'dist/assets/img/{,*/}*.*'
                 ]
             }
         },
 
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded',
-                    sourcemap: true
-                },    
-                files: [{
-                    expand: true,
-                    cwd: 'app/assets/css',
-                    src: ['style.scss'],
-                    dest: 'app/assets/css',
-                    ext: '.css'
-                }]
-            },
-            compressed: {
-                options: {
-                    style: 'compressed'
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'app/assets/css',
-                    src: ['style.scss'],
-                    dest: 'app/assets/css',
-                    ext: '.css'
-                }]
-            }
-        },
-
         connect: {
             server: {
-
                 options: {
                     port: 8000,
-                    // change this to '0.0.0.0' or '*' to access the server from outside
                     hostname: '0.0.0.0',
                     base: './'
                 }
@@ -85,45 +92,12 @@ module.exports = function(grunt) {
             }
         },
 
-        assemble: {
-            options: {
-                flatten: true,
-                layout: 'default.hbs',
-                layoutdir: 'app/templates/layouts',
-                assets: 'dist/assets',
-                partials: ['app/templates/pages/*.hbs', 'app/templates/parts/*.hbs']
-            },
-            dev: {
-                options: {layout: 'default.hbs' },
-                files: {'dist/': ['app/templates/pages/*.hbs' ]},
-            },
-            deploy: {
-                options: {layout: 'default--deploy.hbs' },
-                files: {'dist/': ['app/templates/pages/*.hbs' ]},
-            }
-        },
-
-        clean: {
-            build: {
-                src: ["./dist"]
-            },
-            inuit: {
-                src: ["./dist/assets/css/inuit.css/"]
-            }
-        },
-
         copy: {
-            predeploy: {
+            deploy: {
                 files: [
-                    { expand: true, cwd: './app/assets/css', src: ['./**/*.css'], dest: 'dist/assets/css' },
                     { expand: true, cwd: './app/assets/js', src: ['./vendor/*.*'], dest: 'dist/assets/js' },
                     { expand: true, cwd: './app/assets/img', src: ['./**/*.*'], dest: 'dist/assets/img' },
                     { expand: true, cwd: './app/assets/fonts', src: ['./**/*.*'], dest: 'dist/assets/fonts' }
-                ]
-            },
-            css: {
-                files: [
-                    { expand: true, cwd: './app/assets/css', src: ['./**/*.*'], dest: 'dist/assets/css' }
                 ]
             },
             js: {
@@ -143,50 +117,6 @@ module.exports = function(grunt) {
             }
         },
 
-        concat: {
-            options: {
-                separator: ' ',
-            },
-            dist: {
-                src: ['./app/assets/js/plugins/plugins.js', './app/assets/js/app/main.js'],
-                dest: 'dist/assets/js/script.js',
-            },
-        },
-
-        autoshot: {
-            default_options: {
-                options: {
-                    path: './test/screenshots',
-                    remote: {
-                        files: [
-                            { src: "", dest: "" }
-                        ]
-                    },
-                    local: {
-                        path: './dist',
-                        port: 8000,
-                        files: [
-                            { src: "index.html", dest: "screenshot.jpg" }
-                        ]
-                    },
-                    viewport: [
-                        '1920x1080',
-                        '1200x800',
-                        '1024x768',
-                        '800x600',
-                        '768x1024',
-                        '480x800'
-                    ]
-                },
-            },
-        },
-
-        concurrent: {
-            dev: [
-                'sass'
-            ]
-        },
-
         sprite:{
             all: {
                 src: 'app/assets/img/css/sprites/src/*.png',
@@ -200,19 +130,51 @@ module.exports = function(grunt) {
                     }
               }
             }
+        },
+
+        concat: {
+            options: {
+                separator: ' ',
+            },
+            dist: {
+                src: ['./app/assets/js/plugins/plugins.js', './app/assets/js/app/main.js'],
+                dest: 'dist/assets/js/script.js',
+            },
+        },
+
+        clean: {
+            build: {
+                src: ["./dist"]
+            }
+        },
+
+        concurrent: {
+            dev: [
+                'sass'
+            ]
         }
 
     });
 
-    // Load the plugins.
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-    grunt.loadNpmTasks('assemble');
-    grunt.loadNpmTasks('grunt-autoshot');
 
-    // Initial dev task, opens the site in the browser.
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('assemble');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+
+    // Initial dev task, cleans /dist, opens the site in the browser.
     grunt.registerTask('init', [
         'clean:build',
-        'copy:css',
         'copy:js',
         'copy:img',
         'copy:fonts',
@@ -225,8 +187,6 @@ module.exports = function(grunt) {
 
     // Default dev task without open.
     grunt.registerTask('default', [
-        'clean:build',
-        'copy:css',
         'copy:js',
         'copy:img',
         'copy:fonts',
@@ -240,41 +200,32 @@ module.exports = function(grunt) {
     grunt.registerTask('deploy', [
         'clean:build',
         'sass:compressed',
-        'copy:predeploy',
-        'sass:dist',
+        'copy:deploy',
         'assemble:deploy',
-        'concat',
-        'clean:inuit'
-    ]);
-
-    // Autoshot task, taking screenshots of the various screensizes
-    grunt.registerTask('shot', [
-        'clean:build',
-        'sass:compressed',
-        'copy:predeploy',
-        'sass:dist',
-        'assemble:deploy',
-        'concat',
-        'clean:inuit',
-        'autoshot'
+        'concat'
     ]);
 
     grunt.registerTask('scss', [
-        'sass:dist',
-        'copy:css'
+        'sass:dev'
     ]);
+
     grunt.registerTask('html', [
         'assemble:dev'
     ]);
+
     grunt.registerTask('js', [
         'copy:js'
     ]);
+
     grunt.registerTask('img', [
         'copy:img'
     ]);
+
     grunt.registerTask('copysprite', [
         'sprite',
         'copy:img'
     ]);
+
+
 
 };
---
+++ app/assets/js/app/main.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+	// $('body').css("background", "blue");
+});
