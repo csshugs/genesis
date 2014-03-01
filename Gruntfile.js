39e5212c2c75adf4b00505545273def96d1be730
Florian Uhlig <florianuhlig@outlook.com>
2014-03-01 18:26:41 +0100

    [FEATURE] add grunt-todos

--- Gruntfile.js
+++ Gruntfile.js
@@ -197,6 +197,24 @@ module.exports = function(grunt) {
             build: ['build/assets/js/script.js']
         },
 
+        todos: {
+            options: {
+                priorities: {
+                    low: /(REFACTOR|TODO:low)/,
+                    med: /(FIXME|TODO:med)/,
+                    high: /(BUG|TODO:high)/
+                },
+            verbose: false
+            },
+
+            scripts: {
+                src: ['./app/assets/**/*.js']
+            },
+            style: {
+                src: ['./app/assets/**/*.scss', './app/assets/**/*.css']
+            }
+        },
+
         processhtml: {
             options: {
                 data: {
---
+++ app/assets/todo.js
@@ -0,0 +1,7 @@
+// REFACTOR     genesis.script refactor
+// FIXME        genesis.script todo
+// BUG          genesis.script fixme
+
+// TODO:low     script.low
+// TODO:med     script.med
+// TODO:high    script.high
