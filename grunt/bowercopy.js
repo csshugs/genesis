da6d0fc62d635ee3bcaa5e34a04f705bcf616a06
Dennis Heibült <dennis@csshugs.com>
2015-07-09 12:07:12 +0200

    update bowercopy config file

--- grunt/bowercopy.js
+++ grunt/bowercopy.js
@@ -1,14 +1,14 @@
 module.exports = {
     libs: {
         src: [
-
+            'jquery/dist/jquery.js'
         ],
         dest: '<%= globalConfig.source.js %>/vendor/libs'
     },
-    plugins: {
-        src: [
+    // plugins: {
+    //     src: [
 
-        ],
-        dest: '<%= globalConfig.source.js %>/vendor/plugins'
-    }
+    //     ],
+    //     dest: '<%= globalConfig.source.js %>/vendor/plugins'
+    // }
 };
